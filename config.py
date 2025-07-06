df4e9eed30ccebb11f474c54f3245bfa6e50170a
Nehemaib <nehemiabzwj@gmail.com>
2025-07-07 00:16:02 +0800

    langgraph开发

---
+++ config.py
@@ -0,0 +1,7 @@
+API_KEY = {
+    "service_1": "your_api_key_here",
+    "service_2": "your_api_key_here",
+}
+URLS = {
+    "weather": "https://api.open-meteo.com/v1/forecast"  # https://open-meteo.com/en/docs
+}
